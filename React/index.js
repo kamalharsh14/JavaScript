21ac10b392cc0df6c19d266547da6c704ee5d6ad
Harsh Kamal Singh <singhharsh998@gmail.com>
2021-12-27 22:18:31 +0530

    JS

--- React/index.js
+++ React/index.js
@@ -9,13 +9,28 @@
 // console.error("This is an Error")
 
 // 3. JavaScript Variables
-// let name = 'Harsh';          //String Literal
-// let age = 30;                //Number Literal
-// let isAproved = false;       //Boolean Literal
-// let firstName = undefined;   //Undefined
-// let lastName = null;         //Null
+// let name = 'Harsh'; //String Literal
+// let age = 21; //Number Literal
+// let isAproved = false; //Boolean Literal
+// let firstName = undefined; //Undefined
+// let lastName = null; //Null
 // console.log(name);
 // console.log(age);
 // console.log(isAproved);
 // console.log(firstName);
 // console.log(lastName);
+
+//4. Objects
+// let person = {
+//     name: 'Harsh',
+//     age: 31
+// };
+// console.log(person);
+
+// // Dot Notation
+// person.age = 23;
+// person.name = 'Yash';
+
+// // Bracket Notation
+// person['Yash'] = 'Harsh';
+// person[23] = 21;
