46213c33b929e82ed4a778d3f374fb219cfef2e8
Harsh Kamal Singh <singhharsh998@gmail.com>
2021-12-27 22:21:02 +0530

    JS

--- React/index.js
+++ React/index.js
@@ -34,3 +34,10 @@
 // // Bracket Notation
 // person['Yash'] = 'Harsh';
 // person[23] = 21;
+
+//5. Arrays
+let arr = ['abc', 'xyz', 'qwe'];
+console.log(arr[0]);
+console.log(arr[1]);
+arr[2] = 'fgh';
+console.log(arr[2]);
