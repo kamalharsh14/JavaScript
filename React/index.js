9982e74405d919c0cbae17b164f7cab58df6124d
Harsh Kamal Singh <singhharsh998@gmail.com>
2021-12-27 22:07:09 +0530

    JavaScript

--- React/index.js
+++ React/index.js
@@ -1,11 +1,21 @@
 // 1. Ways to print in JavaScript
-console.log("Hello world")
-alert("This is an Alert")
-document.write("This is Document.Write")
+// console.log("Hello world")
+// alert("This is an Alert")
+// document.write("This is Document.Write")
 
 // 2. JavaScript Console API
-console.log("Hello world")
-console.warn("This is a warning")
-console.error("This is an Error")
+// console.log("Hello world")
+// console.warn("This is a warning")
+// console.error("This is an Error")
 
 // 3. JavaScript Variables
+// let name = 'Harsh';          //String Literal
+// let age = 30;                //Number Literal
+// let isAproved = false;       //Boolean Literal
+// let firstName = undefined;   //Undefined
+// let lastName = null;         //Null
+// console.log(name);
+// console.log(age);
+// console.log(isAproved);
+// console.log(firstName);
+// console.log(lastName);
