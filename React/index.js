5cafe1552ef73836da412d72aec1a76ae14b591d
Harsh Kamal Singh <singhharsh998@gmail.com>
2021-12-29 15:11:38 +0530

    JS

--- React/index.js
+++ React/index.js
@@ -121,31 +121,23 @@
 //         this.lastName = parts[2];
 //     }
 // };
-
 // console.log(person.fullName);
-
 // person.fullName = 'Yash Kamal Singh';
 // console.log(person);
 
 // 14. Value/Reference Types
-
 //Call By Reference
 // let number = { value: 10 };
-
 // function increase(num) {
 //     num.value++;
 // }
-
 // increase(number);
 // console.log(number);
-
 //Call by Value
 // let number = 10;
-
 // function increase(number) {
 //     number++;
 // }
-
 // increase(number);
 // console.log(number);
 
