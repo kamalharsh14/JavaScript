25f356e907eb6e74db10597ffd52ed1d8f594cec
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-07 22:31:44 +0530

    Functions

---
+++ React/Functions.js
@@ -0,0 +1,10 @@
+// 6. Fucntions
+function display() {
+    console.log('hello world');
+}
+display(); // function Calling
+
+function displayname(name) {
+    console.log('hello ' + name);
+}
+displayname('Harsh'); // function Calling
