ec67197febc39e58db1e704802ab7021a75fdddc
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-17 10:16:25 +0530

    Objects

---
+++ JavaScript/JavaScript/Objects.js
@@ -0,0 +1,14 @@
+// 4. Objects
+let person = {
+    name: 'Harsh',
+    age: 31
+};
+console.log(person);
+
+// Dot Notation
+person.age = 23;
+person.name = 'Yash';
+
+// Bracket Notation
+person['Yash'] = 'Harsh';
+person[23] = 21;
