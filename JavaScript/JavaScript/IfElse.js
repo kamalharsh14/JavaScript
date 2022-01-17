be3fe1836e47b0d49840b06c9b0299e4be6b31d5
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-17 10:15:47 +0530

    If Else

---
+++ JavaScript/JavaScript/IfElse.js
@@ -0,0 +1,9 @@
+// 9. If - Else
+let hour = 20;
+if (hour > 0 && hour <= 12) {
+    console.log('Good Morning!');
+} else if (hour > 12 && hour <= 6) {
+    console.log('Good Afternoon!');
+} else {
+    console.log('Good Evening!');
+}
