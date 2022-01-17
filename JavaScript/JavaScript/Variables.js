ee6d68f0ce4fd5535da63cdf5a6f223d4bde20e8
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-01-17 10:17:31 +0530

    Variables

---
+++ JavaScript/JavaScript/Variables.js
@@ -0,0 +1,11 @@
+// 3. JavaScript Variables
+let name = 'Harsh'; //String Literal
+let age = 21; //Number Literal
+let isAproved = false; //Boolean Literal
+let firstName = undefined; //Undefined
+let lastName = null; //Null
+console.log(name);
+console.log(age);
+console.log(isAproved);
+console.log(firstName);
+console.log(lastName);
